2755eaee10868ab982e25ea153aa18781c67fa69
emehta <mehta.ekta@gmail.com>
2015-10-19 09:06:42 -0400

    Added missing files

---
+++ platforms/ios/www/js/controllers/prodCtrl.js
@@ -0,0 +1,31 @@
+//angular.module('iRent.Controllers', [])
+angular.module('iRent')
+.controller('prodCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http){
+	$scope.serviceRoot = iRentApp.serviceRoot+"/";
+    //$http.get(iRentApp.serviceRoot + '/iRentService/product/all').
+	//$http.get(iRentApp.serviceRoot + '/iRentService/product/Prod1').
+		//success(function(data) {
+	console.log("=========="+$scope.serviceRoot)
+	$http.get(iRentApp.serviceRoot + '/iRentService/product/all').
+	        success(function(response) {
+	        	console.log("=========="+iRentApp.serviceRoot + '/iRentService/product/all')
+	        	console.log('prod Data -&', response)
+	            //$scope.prod = data;
+	        	$scope.prods = response;
+	        	console.log("prod size ="+response.length)
+	        })
+}])
+.controller('prodDetailCtrl', ['$scope', '$rootScope', '$http','$routeParams', function($scope, $rootScope, $http,$routeParams){
+	$scope.serviceRoot = iRentApp.serviceRoot+"/";
+	//alert("Inside detail ctrl")
+	var id = $routeParams.prodId;
+	//alert(id);
+	$http.get(iRentApp.serviceRoot + '/iRentService/product/'+id).
+	        success(function(response) {
+	        	console.log(response)
+	        	//alert(response)
+	        	$scope.prod = response;
+	        	console.log("prod name ="+response.prodName)
+	        })
+	        
+}])
